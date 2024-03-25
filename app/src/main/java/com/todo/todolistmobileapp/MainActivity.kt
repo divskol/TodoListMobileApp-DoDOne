242b5e60ea00c9c8583c49ae4d890de1da7be6ed
Raquel0701 <diana.diaz7196@gmail.com>
2024-03-25 18:49:01 -0500

    Login Basic

--- app/src/main/java/com/todo/todolistmobileapp/MainActivity.kt
+++ app/src/main/java/com/todo/todolistmobileapp/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -23,8 +25,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.todo.todolistmobileapp.ui.theme.TodoListMobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -83,8 +87,24 @@ fun BodyContent() {
             }
         )
 
-        Button(onClick = {  }) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 20.dp),
+            onClick = { }) {
             Text(text = "Iniciar sesión")
+        }
+
+
+        Row {
+            Text(text = "No tienes cuenta", fontSize = 14.sp, color = Color.Gray)
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = "Registrate aquí",
+                fontSize = 14.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
